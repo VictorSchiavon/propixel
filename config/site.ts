4d1776b57df83e5630c488e04bb2c8d82ee0b445
VictorSchiavon <36873119+VictorSchiavon@users.noreply.github.com>
2024-12-08 19:22:22 -0300

    MTA

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Raze Host",
+  name: "RazeHost",
   description: "A RazeHost oferece serviços para potencializar seus serviços digitais para tudo que seu projeto precisa e uma equipe que está sempre disposta a te levar ao sucesso.",
   navItems: [
 		{
