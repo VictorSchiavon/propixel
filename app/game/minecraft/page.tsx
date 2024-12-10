c05282b0b3f0db71719918aa629976e45b2d2931
VictorSchiavon <36873119+VictorSchiavon@users.noreply.github.com>
2024-12-10 13:32:53 -0300

    Update page.tsx

--- app/game/minecraft/page.tsx
+++ app/game/minecraft/page.tsx
@@ -20,7 +20,7 @@ export default function Games() {
     return (
         <section className="container mx-auto px-6 flex-grow pt-[2%]">
             <div className="flex w-full flex-col">
-                {displayGame ? <><TabsComponent displayGame={displayGame}/></> : ""}
+                {displayGame ? <><TabsComponent displayGame={displayGame}/></> : "Host de Minecraft"}
             </div>
 
             <section className="pt-10">
