0bc8ffae4be92a1d0f9c222500799be7854fcb2a
VictorSchiavon <36873119+VictorSchiavon@users.noreply.github.com>
2024-12-26 22:25:47 -0300

    Update page.tsx

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -155,7 +155,7 @@ export default function Games() {
                                 spaceBetween: 50,
                             },
                             1536: {
-                                slidesPerView: 14,
+                                slidesPerView: 6,
                                 spaceBetween: 30,
                             },
                         }}
