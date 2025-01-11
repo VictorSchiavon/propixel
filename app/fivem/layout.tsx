a2df9d8fb6f26f1695848fc47c90be4bce8013e7
VictorSchiavon <36873119+VictorSchiavon@users.noreply.github.com>
2025-01-11 14:59:10 -0300

    Update layout.tsx

--- app/fivem/layout.tsx
+++ app/fivem/layout.tsx
@@ -15,6 +15,8 @@ export const metadata: Metadata = {
     "RazeHost Brasil",
     "RazeHost Games",
     "RazeHost VPS",
+    "vps fivem",
+    "vps fivem brasil",
     "host de FiveM",
     "hospedagem de FiveM",
     "FiveM Brasil",
