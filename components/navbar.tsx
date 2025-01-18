e12d2c70a87a55b261ba5d159dd396dda6b4a140
VictorSchiavon <36873119+VictorSchiavon@users.noreply.github.com>
2025-01-18 12:32:17 -0300

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -188,31 +188,31 @@ export const NavbarComponent = () => {
             </PopoverContent>
           </Popover>
         </NavbarItem>
-      <NavbarContent className="flex mt-5 sm:flex" justify="end">
-        <NavbarItem>
-          <NextLink href="https://app.razehost.com.br/login">
-            <Button variant="flat" size="sm" className="text-sm font-normal bg-orange-500 border-0 text-white rounded-full">Acessar minha conta</Button>
-          </NextLink>
-        </NavbarItem>
-        <NavbarItem className="sm:block hidden">
-  <a 
-    href="https://discord.gg/p8YXcEuKdH" 
-    target="_blank" 
-    rel="noopener noreferrer" 
-    className="flex items-center justify-center p-0 m-0"
-  >
-    <Image 
-      src="/discord.png" 
-      alt="Discord" 
-      width={20} 
-      height={20} 
-      className="p-0 m-0"
-    />
-  </a>
-</NavbarItem>
+ {/* Novo Menu Contato */}
+<NavbarContent className="flex mt-5 sm:flex" justify="end">
+  <NavbarItem>
+    <NextLink href="https://app.razehost.com.br/login">
+      <Button variant="flat" size="sm" className="text-sm font-normal bg-orange-500 border-0 text-white rounded-full">Acessar minha conta</Button>
+    </NextLink>
+  </NavbarItem>
+  <NavbarItem className="sm:block hidden">
+    <a 
+      href="https://discord.gg/p8YXcEuKdH" 
+      target="_blank" 
+      rel="noopener noreferrer" 
+      className="flex items-center justify-center p-0 m-0"
+    >
+      <Image 
+        src="/discord.png" 
+        alt="Discord" 
+        width={20} 
+        height={20} 
+        className="p-0 m-0"
+      />
+    </a>
+  </NavbarItem>
+</NavbarContent>
 
-
-      </NavbarContent>
       <NavbarMenu>
         <NavbarItem key="01" className="mt-4">
           <NextLink href="/" className="p-0">
