09b3ec089146f3ff2104637979adc3c43c1ed216
VictorSchiavon <36873119+VictorSchiavon@users.noreply.github.com>
2025-01-18 12:59:05 -0300

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -64,7 +64,7 @@ export const NavbarComponent = () => {
                     {[
                       { href: "/vps-trader", title: "VPS Trader", description: "Servidor veloz para traders." },
                       { href: "/vps-flex", title: "VPS Flex", description: "Servidores custo-benefício único." },
-                      { href: "/fivem", title: "VPS FiveM", description: "Servidores pra FiveM e base grátis." },
+                      { href: "/colocation", title: "Colocation", description: "Hospede seu servidor em nossa infraestrutura." },
                       { href: "/semidedicados", title: "Semidedicados", description: "Configurações superiores, para aplicação grandes." }
                     ].map((item, index) => (
                       <Link href={item.href} key={index}>
