e41808b38e52e62ab5a53ae2d391b798234a13fb
VictorSchiavon <36873119+VictorSchiavon@users.noreply.github.com>
2025-04-26 15:15:08 -0300

    Update page.tsx

--- app/vps/page.tsx
+++ app/vps/page.tsx
@@ -104,7 +104,7 @@ export default function Games() {
             <section>
                 <div>
                     <h1 className="text-5xl font-bold pt-2 text-center">
-                        VPS Ryzen no Brasil
+                        Servidores VPS no Brasil
                     </h1>
                     <p className="text-muted pt-2 pb-2 text-center">
                         Escolha o plano ideal para seu negócio e evolua conforme suas necessidades.
