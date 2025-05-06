6580fd48658ba5846eef29a5e45256d88a776a96
VictorSchiavon <36873119+VictorSchiavon@users.noreply.github.com>
2025-05-06 18:51:36 -0300

    Update page.tsx

--- app/dedicados/page.tsx
+++ app/dedicados/page.tsx
@@ -118,7 +118,7 @@ const planosAMD = [
       { icone: <Cpu size={30} />, titulo: "16 Núcleos", descricao: "32 Threads" },
       { icone: <HardDrive size={30} />, titulo: "Armazenamento", descricao: "2TB DE SSD NVME M.2" },
       { icone: <Network size={30} />, titulo: "Rede", descricao: "1GBPS UPLINK" },
-      { icone: <MemoryStick size={30} />, titulo: "128GB DE RAM", descricao: "DDR5" },
+      { icone: <MemoryStick size={30} />, titulo: "192GB DE RAM", descricao: "DDR5" },
       { icone: <ArrowUpFromDot size={30} />, titulo: "Inputstream", descricao: "Tráfego ilimitado" },
       { icone: <ArrowDownToDot size={30} />, titulo: "Outputstream", descricao: "30Tb Incluso" },
     ],
