bd845440387a2b126d72d88e76e5bc99d0df2517
VictorSchiavon <36873119+VictorSchiavon@users.noreply.github.com>
2025-05-09 13:14:40 -0300

    Update page.tsx

--- app/game/armareforger/page.tsx
+++ app/game/armareforger/page.tsx
@@ -1,23 +1,23 @@
-"use client"
-import games from "@/config/games.json"
-import "swiper/css"
-import "swiper/css/navigation"
-import "swiper/css/pagination"
-import { useEffect, useState } from "react"
-import { Card, CardBody, Divider } from "@nextui-org/react"
-import Link from "next/link"
-import CircularProgress from "../_components/circular-progress.component"
-import { TabsComponent } from "../_components/tabs-component"
+"use client";
+import games from "@/config/games.json";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { useEffect, useState } from "react";
+import { Card, CardBody, Divider } from "@nextui-org/react";
+import Link from "next/link";
+import CircularProgress from "../_components/circular-progress.component";
+import { TabsComponent } from "../_components/tabs-component";
 
 export default function Games() {
-  const [displayGame, setDisplayGame] = useState<any>()
+	const [displayGame, setDisplayGame] = useState<any>();
 
-  useEffect(() => {
-    const findGame = games.filter((item) => item.id === 20)
-    setDisplayGame(findGame[0])
-  }, [])
+	useEffect(() => {
+		const findGame = games.filter((item) => item.id === 20);
+		setDisplayGame(findGame[0]);
+	}, []);
 
-  return (
+	return (
 		<section className="container mx-auto px-6 flex-grow pt-[2%]">
 			<div className="flex w-full flex-col">
 				{displayGame ? (
@@ -30,50 +30,28 @@ export default function Games() {
 			</div>
 
 			<section className="pt-10">
-				<h3 className="font-bold text-2xl mb-6">Notas e avaliações:</h3>
+				<h3 className="font-bold text-2xl">Notas e avaliações:</h3>
 
-				<div className="grid grid-cols-1 md:grid-cols-3 gap-8 pt-5">
-					<div className="bg-gradient-to-b from-[#0E131D] to-[#1A2030] p-6 rounded-xl shadow-md hover:shadow-lg transition-all duration-300">
+				<div className="flex gap-8 pt-5">
+					<div>
 						<CircularProgress percentage={100} />
-						<p className="text-center text-sm text-gray-400 mt-4">
+						<p className="text-center text-sm text-gray-500">
 							Classificação do Google
 						</p>
-                        <div className="flex justify-center mt-2">
-                            {[1, 2, 3, 4, 5].map((star) => (
-                                <svg key={star} xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-yellow-400" viewBox="0 0 20 20" fill="currentColor">
-                                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                                </svg>
-                            ))}
-                        </div>
 					</div>
-					<div className="bg-gradient-to-b from-[#0E131D] to-[#1A2030] p-6 rounded-xl shadow-md hover:shadow-lg transition-all duration-300">
+					<div>
 						<CircularProgress percentage={100} />
-						<p className="text-center text-sm text-gray-400 mt-4">
+						<p className="text-center text-sm text-gray-500">
 							Classificação do Trustpilot
 						</p>
-                        <div className="flex justify-center mt-2">
-                            {[1, 2, 3, 4, 5].map((star) => (
-                                <svg key={star} xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-yellow-400" viewBox="0 0 20 20" fill="currentColor">
-                                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                                </svg>
-                            ))}
-                        </div>
 					</div>
-					<div className="bg-gradient-to-b from-[#0E131D] to-[#1A2030] p-6 rounded-xl shadow-md hover:shadow-lg transition-all duration-300">
+					<div>
 						<CircularProgress percentage={100} />
-						<p className="text-center text-sm text-gray-400 mt-4">
+						<p className="text-center text-sm text-gray-500">
 							Classificação do Discord
 						</p>
-                        <div className="flex justify-center mt-2">
-                            {[1, 2, 3, 4, 5].map((star) => (
-                                <svg key={star} xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-yellow-400" viewBox="0 0 20 20" fill="currentColor">
-                                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                                </svg>
-                            ))}
-                        </div>
 					</div>
 				</div>
-			</section>
 
 				<section className="pt-20">
 					<div className="flex justify-end">
@@ -144,6 +122,6 @@ export default function Games() {
 					</p>
 				</div>
 			</section>
-  </section>
-	)
+		</section>
+	);
 }
